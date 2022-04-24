f035ae03171502c14bbe6cb20be3b446bd3098e8
Muhammad Umair <49072162+itskanny@users.noreply.github.com>
2022-04-24 22:08:19 +0500

    Project Created

--- annolab/projects/api/views.py
+++ annolab/projects/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import MultiPartParser, JSONParser
 
 from .serializer import ProjectsSerializer
 from ..models import Project
@@ -8,4 +8,9 @@ from ..models import Project
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectsSerializer
     queryset = Project.objects.all()
-    parser_classes = [MultiPartParser]
+    parser_classes = [MultiPartParser, JSONParser]
+
+    def get_queryset(self, *args, **kwargs):
+        assert isinstance(self.request.organization.id, int)
+
+        return self.queryset.filter(organization=self.request.organization.id)
--- annolab/projects/models.py
+++ annolab/projects/models.py
@@ -6,7 +6,8 @@ from annolab.organizations.models import Organization
 # Create your models here.
 
 class Project(models.Model):
-    name = models.CharField(max_length=255, unique=True, verbose_name='Projects')
+    name = models.CharField(max_length=255, unique=True, verbose_name='Project Name')
     description = models.TextField(verbose_name='Project Description')
     avatar = models.ImageField(upload_to='project_avatars', blank=True, null=True)
     organization = models.ForeignKey(Organization, related_name='projects', on_delete=models.CASCADE)
+
